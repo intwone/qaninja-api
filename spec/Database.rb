ccebf3abcc573ced2a599775671eb041b8c852c8
Ca#9874123 <cassiointw1993@gmail.com>
2020-01-25 00:40:44 -0300

    [ADD]findUser method

--- spec/Database.rb
+++ spec/Database.rb
@@ -6,6 +6,14 @@ class Database
     @connection = PG.connect(dbConnection)
   end
 
+  def findUser(email)
+    query = "SELECT id, full_name, password, email, created_at, updated_at" \
+            " FROM public.users WHERE email = '#{email}';"
+
+    # first retorna a primeira posição do objeto
+    @connection.exec(query).first
+  end
+
   def deleteUser(email)
     query = "DELETE FROM public.users WHERE email = '#{email}';"
     @connection.exec(query)
--- spec/get_spec.rb
+++ spec/get_spec.rb
@@ -3,7 +3,13 @@ describe "get" do
     let (:user) { build(:registeredUser) }
     let (:token) { ApiUser.token(user.email, user.password) }
     let (:result) { ApiUser.find(token, user.id) }
+    let (:userData) { Database.new.findUser(user.email) }
 
     it { expect(result.response.code).to eql "200" }
+    it { expect(result.parsed_response["full_name"]).to eql userData["full_name"] }
+    it { expect(result.parsed_response["password"]).to eql userData["password"] }
+    it { expect(result.parsed_response["email"]).to eql userData["email"] }
+    it { expect(Time.parse(result.parsed_response["createdAt"])).to eql Time.parse(userData["created_at"]) }
+    it { expect(Time.parse(result.parsed_response["updatedAt"])).to eql Time.parse(userData["updated_at"]) }
   end
 end
