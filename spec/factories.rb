e8cbeb0c2fe989fc72ab7008269c9206ce8482ce
Ca#9874123 <cassiointw1993@gmail.com>
2020-01-24 23:04:56 -0300

    [REF]testing duplicate email

--- spec/factories.rb
+++ spec/factories.rb
@@ -11,6 +11,17 @@ FactoryBot.define do
     end
   end
 
+  factory :duplicateEmail, class: UserModel do
+    full_name { "Papito Skywalker" }
+    email { "papito@jedi.com" }
+    password { "jedi123" }
+
+    after(:build) do |user|
+      Database.new.deleteUser(user.email)
+      ApiUser.save(user.toHash)
+    end
+  end
+
   factory :userWrongEmail, class: UserModel do
     full_name { "Papito Skywalker" }
     email { "papito#edi.com" }
@@ -33,12 +44,6 @@ FactoryBot.define do
     full_name { "Cassio Oliveira" }
     email { "cassio@gmai.com" }
     password { "" }
-  end
-
-  factory :duplicateEmail, class: UserModel do
-    full_name { "Tony Stark" }
-    email { "tony@stark.com" }
-    password { "pwd123" }
   end
 
   factory :nullEmail, class: UserModel do
