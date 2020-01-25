3bb79335ef2fb56736a6e80e70262f8953af241a
Ca#9874123 <cassiointw1993@gmail.com>
2020-01-24 22:47:34 -0300

    [ADD]testing error 4xx

--- spec/factories.rb
+++ spec/factories.rb
@@ -16,4 +16,34 @@ FactoryBot.define do
     email { "papito#edi.com" }
     password { "jedi123" }
   end
+
+  factory :emptyUserName, class: UserModel do
+    full_name { "" }
+    email { "papito1@jedi.com" }
+    password { "jedi123" }
+  end
+
+  factory :emptyUserEmail, class: UserModel do
+    full_name { "Cassio Oliveira" }
+    email { "" }
+    password { "jedi123" }
+  end
+
+  factory :emptyPassword, class: UserModel do
+    full_name { "Cassio Oliveira" }
+    email { "cassio@gmai.com" }
+    password { "" }
+  end
+
+  factory :duplicateEmail, class: UserModel do
+    full_name { "Tony Stark" }
+    email { "tony@stark.com" }
+    password { "pwd123" }
+  end
+
+  factory :nullEmail, class: UserModel do
+    full_name { "Tony Stark" }
+    email { }
+    password { "pwd123" }
+  end
 end
--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -11,17 +11,60 @@ describe "post" do
   context "when wrong email" do
     before do
       @newUser = build(:userWrongEmail).toHash
-
-      @result = HTTParty.post(
-        "http://localhost:3001/user",
-        body: @newUser.to_json,
-        headers: {
-          "Content-Type" => "application/json",
-        },
-      )
+      @result = ApiUser.save(@newUser)
     end
 
     it { expect(@result.response.code).to eql "412" }
     it { expect(@result.parsed_response["msg"]).to eql "Oops. You entered a wrong email." }
+  end
+
+  context "when empty name" do
+    before do
+      @newUser = build(:emptyUserName).toHash
+      @result = ApiUser.save(@newUser)
+    end
+
+    it { expect(@result.response.code).to eql "412" }
+    it { expect(@result.parsed_response["msg"]).to eql "Validation notEmpty on full_name failed" }
+  end
+
+  context "when empty email" do
+    before do
+      @newUser = build(:emptyUserEmail).toHash
+      @result = ApiUser.save(@newUser)
+    end
+
+    it { expect(@result.response.code).to eql "412" }
+    it { expect(@result.parsed_response["msg"]).to eql "Validation notEmpty on email failed" }
+  end
+
+  context "when empty password" do
+    before do
+      @newUser = build(:emptyPassword).toHash
+      @result = ApiUser.save(@newUser)
+    end
+
+    it { expect(@result.response.code).to eql "412" }
+    it { expect(@result.parsed_response["msg"]).to eql "Validation notEmpty on password failed" }
+  end
+
+  context "when duplicate email" do
+    before do
+      @newUser = build(:duplicateEmail).toHash
+      @result = ApiUser.save(@newUser)
+    end
+
+    it { expect(@result.response.code).to eql "409" }
+    it { expect(@result.parsed_response["msg"]).to eql "Oops. Looks like you already have an account with this email address." }
+  end
+
+  context "when null email" do
+    before do
+      @newUser = build(:nullEmail).toHash
+      @result = ApiUser.save(@newUser)
+    end
+
+    it { expect(@result.response.code).to eql "412" }
+    it { expect(@result.parsed_response["msg"]).to eql "Users.email cannot be null" }
   end
 end
